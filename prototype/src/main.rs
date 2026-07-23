68c5602ddb01dc13479f41cb4abee4b9d48e9dc6
dennis.pruene <dennis.pruene@rwth-aachen.de>
2026-07-23 15:55:48 +0900

    add tree sitter dependency

--- prototype/src/main.rs
+++ prototype/src/main.rs
@@ -6,9 +6,21 @@ mod chain;
 use crate::ast::Chain;
 use crate::chain::ChainParser;
 
+use tree_sitter::{InputEdit, Language, Parser, Point};
+
 fn main() {
-    let chain: Chain = ChainParser::new()
-        .parse(r#"select("*").from("Students")"#)
-        .unwrap();
-    println!("{chain}");
+    let mut parser = Parser::new();
+    parser.set_language(&tree_sitter_java::LANGUAGE.into()).expect("Error loading Java grammar");
+
+    let source_code =
+r#"public class Example {
+    public void main(String[] args) {
+        Query query = SQL.select("*").from("Students").where("age >= 18").build();
+    }
+}"#;
+    println!("{source_code}");
+
+    let mut tree = parser.parse(source_code, None).unwrap();
+    let root_node = tree.root_node();
+    println!("{root_node:#?}");
 }
