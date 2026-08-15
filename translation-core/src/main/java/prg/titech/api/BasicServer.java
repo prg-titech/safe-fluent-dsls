cad343352fe5c1d322bb21c77e5431b0ef87b6d5
dennis.pruene <dennis.pruene@rwth-aachen.de>
2026-08-16 01:59:17 +0900

    add very basic server

---
+++ translation-core/src/main/java/prg/titech/api/BasicServer.java
@@ -0,0 +1,42 @@
+package prg.titech.api;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.net.InetSocketAddress;
+import java.util.Optional;
+
+public class BasicServer {
+    public static void main(String[] args) throws Exception {
+        Options options = parseOptions(args);
+        HttpServer server = HttpServer.create(new InetSocketAddress(options.port().orElse(8080)), 0);
+        server.createContext("/health", new HealthHandler());
+        server.setExecutor(null); // default executor
+        server.start();
+        System.out.println("BACKEND_PORT " + server.getAddress().getPort());
+    }
+
+    private static Options parseOptions(String[] args) {
+        Optional<Integer> port = Optional.empty();
+        // If the last entry in arg is "--port", then the actual port _number_ is unspecified!
+        for (int i = 0; i < args.length - 1; i++) {
+            if (args[i].equals("-p") || args[i].equals("--port")) {
+                port = Optional.of(Integer.parseInt(args[i + 1]));
+            }
+        }
+        return new Options(port);
+    }
+
+    private record Options(Optional<Integer> port) { }
+
+    static class HealthHandler implements HttpHandler {
+        public void handle(HttpExchange exchange) {
+            try {
+                exchange.sendResponseHeaders(200, -1);
+            } catch (Exception e) {
+                e.printStackTrace(System.err);
+            }
+        }
+    }
+}
